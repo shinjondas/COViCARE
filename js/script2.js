6224023ddc567968d1ec12ac0bc119cdbb1d2a1a
shinjondas <shinjon12@gmail.com>
2021-04-22 15:00:53 +0530

    Bug fixed

--- js/script2.js
+++ js/script2.js
@@ -4,6 +4,7 @@ var text;
 var k=0;
 function passvalue2(){
     document.getElementById("data").innerHTML="";
+    k=0;
     text="";
     str="";
     text=document.getElementById("input-text").value;
@@ -11,12 +12,13 @@ function passvalue2(){
         if (this.readyState == 4 && this.status == 200) {
             console.log(text);
             var myArr = JSON.parse(this.responseText);
-            console.log(myArr);
+            //console.log(myArr);
             while(k!=myArr.length){
                 if(myArr[k].district==text){
                     str="<tr>";
                     str+="<td>";
                     str+=myArr[k].district+"";
+                    //console.log(myArr[k].district);
                     str+="</td>";
                     str+="<td>";
                     str+=myArr[k].hospname+"";
