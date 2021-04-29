270a3a52bb07e74fc0f6ca7a828150af16c2f095
shinjondas <shinjon12@gmail.com>
2021-04-29 15:10:56 +0530

    Alert box WB

--- php/allinfo.php
+++ php/allinfo.php
@@ -11,10 +11,11 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 <body class="bg-dark">
 <script>
-    alert("Hospitals in WB shall directly provide with Remdesivir!")
+    swal("Hospitals in WB shall directly provide with Remdesivir!");
   </script>
       <div class="jumbotron jumbotron-fluid bg-dark">
         <div class="container">
